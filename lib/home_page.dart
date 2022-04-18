4df75dc4d17a1452b551c173e259e3fd2232d91f
rupesh.shrestha <rupesh.shrestha@fusemachines.com>
2022-04-18 16:21:14 +0545

    refactor

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sports/Screens/widgets/brief_description.dart';
@@ -93,6 +95,7 @@ class HomePage extends StatelessWidget {
                   ),
                   ThumbnailImage(
                       urlToImage: articles[pos].urlToImage.toString()),
+                      
                 ],
               ),
             ),
--- lib/res/strings.dart
+++ lib/res/strings.dart
@@ -1,4 +1,4 @@
 class AppStrings {
   static String cricArticleUrl =
-      "https://newsapi.org/v2/everything?q=tesla&from=2022-03-06&sortBy=publishedAt&apiKey=c86851b92f3d4354afbfbaefde4a6eb3";
+      "https://newsapi.org/v2/everything?q=tesla&from=2022-03-12&sortBy=publishedAt&apiKey=c86851b92f3d4354afbfbaefde4a6eb3";
 }
